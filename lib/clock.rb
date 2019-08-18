aa01bbdf5b7f00b0ac9ee87c61df535bcf8a434e
nekomaho <nekosatoru@gmail.com>
2019-08-19 06:38:04 +0900

    Add console initialize

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
+
+require_relative 'console'
+
 class Clock
   class << self
     def start
+      new
     end
+  end
+
+  def initialize
+    Console.init
   end
 end
--- lib/console.rb
+++ lib/console.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
+require 'curses'
 
 class Console
+  class << self
+    def init
+      Curses.init_screen
+      Curses.start_color
+      Curses.init_pair(1, Curses::COLOR_WHITE, Curses::COLOR_BLUE)
+    end
+  end
 end
